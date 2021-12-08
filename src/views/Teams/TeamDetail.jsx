20ab385de6c7ec9b1b7652f57df7286149406234
KDSigel <heykarl@gmail.com>
2021-12-07 17:05:35 -0800

    added lots of stuff

--- src/views/Teams/TeamDetail.jsx
+++ src/views/Teams/TeamDetail.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { getTeamById } from '../../services/teams'
 
 export default function TeamDetail() {
+
+    const [team, setTeam] = useState({})
+    const [loading, setLoading] = useState(true)
+    const {id} = useParams()
+
+    useEffect(() => {
+        async function getTeamDeets() {
+            const response = await getTeamById(id)
+            setTeam(response)
+            setLoading(false)
+        }
+        getTeamDeets()
+    }, []
+    )
+
     return (
         <div>
-            team detail
+            {loading ? <h1>'loading'</h1>
+                    : <>
+                    <h2>{team.name}</h2>
+                    <p>{team.city}</p>
+                    <p>{team.state}</p>
+                    </>
+            }
         </div>
     )
 }
